9a89db5d51c6aeb84cfa46bb4cd80e8e2a114a19
henriqueserafin7 <henriqueserafin7@gmail.com>
2023-05-23 18:37:27 -0300

    criaçãoescolher opção no menu

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
-#include 
+#include <stdio.h>
+#include "functions.h"
+#include <string.h>
+#include <math.h>
+
+int main() {
+    char opcao;
+
+    AbrirMenu();
+    scanf(" %c", &opcao);
+
+    printf("%c", opcao);
+}
