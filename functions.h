84c4e5c3be9cbbc8ca873b8177e3fbdff18fba27
henriqueserafin7 <henriqueserafin7@gmail.com>
2023-05-23 18:35:16 -0300

    adiconada opcões na functions e menu

--- functions.h
+++ functions.h
@@ -3,3 +3,19 @@
 #include <time.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
+
+const int MAX_USUARIOS = 1000;
+int numUsuarios;
+
+void AbrirMenu(){
+
+    printf("1 - Adicionar Usuario\n");
+    printf("2 - Editar Usuario\n");
+    printf("3 - Excluir usuario\n");
+    printf("4 - Buscar por Email\n");
+    printf("5 - Imprimir todos os usuarios\n");
+    printf("6 - Fazer Backup dos usuarios\n");
+    printf("7 - Restaurar os dados\n");
+    printf("Digite a opcao escolhida: ");
+}
